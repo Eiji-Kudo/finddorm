f4a963d3f1e0796e0f79ad6e7e6112cd1a2ed8fc
Eiji <50960409+justkidding55boy@users.noreply.github.com>
2024-05-03 23:33:56 +0900

    refactor: enable prettier

--- front/src/app/component/title/index.tsx
+++ front/src/app/component/title/index.tsx
@@ -1,29 +1,34 @@
-
-'use client'
-import { Container, Title, Text } from '@mantine/core';
-import styled from '@emotion/styled';
-
+"use client";
+import { Container, Title, Text } from "@mantine/core";
+import styled from "@emotion/styled";
 
 const StyledContainer = styled(Container)`
   padding: 20px;
-  background-color: #f5f5f5; 
+  background-color: #f5f5f5;
   border-radius: 8px;
   box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
 `;
 
 const StyledTitle = styled(Title)`
-  color: #1a73e8; 
+  color: #1a73e8;
   margin-bottom: 16px;
 `;
 
-
 export default function TitleComponent() {
-    return (
-      <StyledContainer>
-        <StyledTitle order={1}>寮・社宅・一棟貸しマンション、アパート物件情報</StyledTitle>
-        <Text style={{ color: '#333', lineHeight: '1.6', marginBottom: '8px' }}>寮・社宅・一括貸しマンション、アパート物件情報をまとめています。</Text>
-        <Text style={{ color: '#333', lineHeight: '1.6', marginBottom: '8px' }}>毎日更新しています。</Text>
-        <Text style={{ color: '#333', lineHeight: '1.6', marginBottom: '8px' }}>気になる物件があればページ下部のお問い合わせフォームからご連絡お願いいたします.</Text>
-      </StyledContainer>
-    );
+  return (
+    <StyledContainer>
+      <StyledTitle order={1}>
+        寮・社宅・一棟貸しマンション、アパート物件情報
+      </StyledTitle>
+      <Text style={{ color: "#333", lineHeight: "1.6", marginBottom: "8px" }}>
+        寮・社宅・一括貸しマンション、アパート物件情報をまとめています。
+      </Text>
+      <Text style={{ color: "#333", lineHeight: "1.6", marginBottom: "8px" }}>
+        毎日更新しています。
+      </Text>
+      <Text style={{ color: "#333", lineHeight: "1.6", marginBottom: "8px" }}>
+        気になる物件があればページ下部のお問い合わせフォームからご連絡お願いいたします.
+      </Text>
+    </StyledContainer>
+  );
 }

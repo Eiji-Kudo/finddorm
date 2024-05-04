5d03074df1ca85bfdfdb7d366beb80aa3117eb8c
Eiji <50960409+justkidding55boy@users.noreply.github.com>
2024-05-04 11:57:17 +0900

    feat: スタイルの中央寄せ

--- front/src/app/component/title/index.tsx
+++ front/src/app/component/title/index.tsx
@@ -2,18 +2,6 @@
 import { Container, Title, Text } from "@mantine/core";
 import styled from "@emotion/styled";
 
-const StyledContainer = styled(Container)`
-  padding: 20px;
-  background-color: #f5f5f5;
-  border-radius: 8px;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-`;
-
-const StyledTitle = styled(Title)`
-  color: #1a73e8;
-  margin-bottom: 16px;
-`;
-
 export default function TitleComponent() {
   return (
     <StyledContainer>
@@ -27,8 +15,24 @@ export default function TitleComponent() {
         毎日更新しています。
       </Text>
       <Text style={{ color: "#333", lineHeight: "1.6", marginBottom: "8px" }}>
-        気になる物件があればページ下部のお問い合わせフォームからご連絡お願いいたします.
+        気になる物件があればページ下部のお問い合わせフォームからご連絡お願いいたします。
       </Text>
     </StyledContainer>
   );
 }
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center; // すべての子要素を中央に寄せます
+  padding: 20px;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const StyledTitle = styled(Title)`
+  color: #1a73e8;
+  margin-bottom: 16px;
+  text-align: center; // タイトルを中央寄せにします
+`;
